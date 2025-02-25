861c1e1164279bbbf05086357df09e84f7fdbdf1
Alex Minzatanu <alexandru.minzatanu@gmail.com>
2025-02-25 11:33:05 +0200

    1. avem principii oop

---
+++ classes/Category.cs
@@ -0,0 +1,9 @@
+namespace SharpShop.Classes
+{
+    public class Category(int id, string name, string description)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
+    }
+}
